9c74a34b46f825029d82f45a3e9cb8e0354b1ce5
Ethan <ethan.zieba@laplateforme.io>
2024-02-03 21:27:06 +0100

    Add all jobs

---
+++ jour01/job05.sql
@@ -0,0 +1,6 @@
+INSERT INTO etudiant (nom, prenom, email, age) VALUES ("Betty", "Spaghetti", "betty.spaghetti@laplateforme.io", 23);
+INSERT INTO etudiant (nom, prenom, email, age) VALUES ("Chuck", "Steak", "chuck.steak@laplateforme.io", 45);
+INSERT INTO etudiant (nom, prenom, email, age) VALUES ("Doe", "John", "john.doe@laplateforme.io", 18);
+INSERT INTO etudiant (nom, prenom, email, age) VALUES ("Binkie", "Barnes", "binkie.barnes@laplateforme.io", 16);
+INSERT INTO etudiant (nom, prenom, email, age) VALUES ("Dupuis", "Gertrude", "gertrude.dupuis@laplateforme.io", 20);
+
